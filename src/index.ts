156d39f2de5b377414064b0eff4d6f56fb45b6db
evaou <speeder528@gmail.com>
2021-03-19 11:55:30 +0800

    add set method

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { User } from './models/User';
 
 console.log('Hi there!');
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({});
+
+user.set({ age: 888 });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
