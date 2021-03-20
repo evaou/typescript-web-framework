3baf5d015a8e4eafb4f61790e1e88dd86aa62a49
evaou <speeder528@gmail.com>
2021-03-20 19:47:26 +0800

    refactor with nested object composition

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'NEW RECORD', age: 0 });
-user.save();
+
+user.events.on('change', () => {
+  console.log('change!');
+});
+
+user.events.trigger('change');
